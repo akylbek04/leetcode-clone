45e81721df73e3139b26010c8ddf17f8af28eb51
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-07 05:08:29 +0200

    fixed bugs

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,4 +1,27 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+// import { initializeApp, getApp, getApps } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+
+// const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+// const auth = getAuth(app);
+// const firestore = getFirestore(app);
+
+// export { auth, firestore, app };
+
+
+
+
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -11,9 +34,31 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 
 export { auth, firestore, app };
+
+
+
+
+
+
+
+
+// import firebase from "firebase/compat/app";
+// import "firebase/auth";
+
+// const app = firebase.initializeApp({
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+//   appId: process.env.FIREBASE_APP_ID,
+// });
+
+// export const auth = app.auth;
+// export default app;
